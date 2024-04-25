ac134aa915c703946afa359e7c5d3adf0203a489
I admin <dirks.anton96@yandex.ru>
2024-04-25 19:31:12 +0300

    add gunicorn, change settings secret key, change image

--- coworkingspace/coworkingspace/settings.py
+++ coworkingspace/coworkingspace/settings.py
@@ -25,12 +25,12 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*&y@d6wekw$ahnnyqr5%-bm7gqhpyo)yk(je0t(h(7*#@=6!j='
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
